0ee5cc33c00d0148eb856e465dadecb3c6829342
ohiosveryown <matt@ohiosveryown.co>
2024-09-01 12:23:43 -0400

    began index list; b4 merge

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,19 @@
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+  <div class="container">
+    <main>
+      <ContentDoc />
+    </main>
+    <List />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+  @import "/assets/style/grid.scss";
+  @import "/assets/style/type.scss";
+
+  .container {
+    @include breakpoint(md) {
+      display: flex;
+    }
+  }
+</style>
